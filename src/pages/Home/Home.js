358c45430737ce15fec6bed2f429cf6dee8ec28e
Walker-mfv <laiphuc2310@gmail.com>
2023-01-02 20:34:47 +0700

    add blog component

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,23 +1,23 @@
-import React from 'react'
-import Greet from './Greet/Greet'
-import About from './About/About'
-import Menu from './Menu/Menu'
-import Blogs from './Blogs/Blogs'
-import Contact from './Contact/Contact'
-import Footer from '../../components/footer/Footer'
-import './styles.css'
+import React from 'react';
+import Greet from './Greet/Greet';
+import About from './About/About';
+import Menu from './Menu/Menu';
+import Blogs from './Blogs/Blogs';
+import Contact from './Contact/Contact';
+import Footer from '../../components/footer/Footer';
+import './styles.css';
 
-const Home = ({setCart}) => {
+const Home = ({ setCart }) => {
   return (
     <div className="home-container">
       <Greet />
       {/* <About /> */}
       <Menu setCart={setCart} />
-      {/* <Blogs /> */}
+      <Blogs />
       {/* <Contact /> */}
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
