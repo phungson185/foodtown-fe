f00b1eaea932793661f97f864ee64f51ad3157aa
truongvx85024 <truongvx85024@co-well.vn>
2023-01-04 15:35:06 +0700

    fixxx

--- src/pages/Auth/Auth.js
+++ src/pages/Auth/Auth.js
@@ -1,9 +1,12 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { useDispatch } from 'react-redux'
 import './styles.css'
 import { signup, login } from '../../actions/auth'
 import { ADMIN } from '../../constants/role'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { isEmpty } from 'lodash'
 
 const Auth = () => {
   const [signupInfo, setSignupInfo] = useState({
@@ -15,6 +18,14 @@ const Auth = () => {
     repeatPassword: '',
   })
 
+  const loginError = useRef({
+    email: '',
+    password: ''
+  })
+  const signupError = useRef({
+    password: ''
+  })
+
   const [loginInfo, setLoginInfo] = useState({
     email: '',
     password: '',
@@ -23,6 +34,42 @@ const Auth = () => {
   const [mode, setMode] = useState(0)
   const navigate = useNavigate()
   const dispatch = useDispatch()
+  const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+
+  //  login validation
+  const validateSignup = useRef(false);
+  const handleValidateSignup = () => {
+    validateSignup.current = false;
+    if (!signupInfo.password.trim()) {
+      signupError.current.password = 'Thiếu password'
+    } else {
+      if (!passwordRegex.test(signupInfo.password)) {
+        signupError.current.password = 'Mật khẩu tối thiểu 8 ký tự, ít nhất 1 chữ hoa, 1 số, 1 ký tự đặc biệt'
+      } else {
+        validateSignup.current = true
+      }
+    }
+  }
+  //  login validation
+  const validateLogin = useRef(false);
+  const handleValidateLogin = () => {
+    validateLogin.current = false;
+    if (!loginInfo.email.trim() || !loginInfo.password.trim()) {
+      if (!loginInfo.email.trim()) {
+        loginError.current.email = 'Thiếu email'
+      }
+      if (!loginInfo.password.trim()) {
+        loginError.current.password = 'Thiếu password'
+      }
+    } else {
+      loginError.current.email = ''
+      if (!passwordRegex.test(loginInfo.password)) {
+        loginError.current.password = 'Mật khẩu tối thiểu 8 ký tự, ít nhất 1 chữ hoa, 1 số, 1 ký tự đặc biệt'
+      } else {
+        validateLogin.current = true
+      }
+    }
+  };
 
   const handleTypingSignupInfo = (e) => {
     const changeElement = e.target
@@ -59,18 +106,64 @@ const Auth = () => {
 
   const onSubmitLogin = (e) => {
     e.preventDefault()
+    handleValidateLogin()
     try {
-      dispatch(login(loginInfo, false, navigate))
+      if (validateLogin.current)
+        dispatch(login(loginInfo, false, navigate))
+      else {
+        toast.dismiss()
+        Object.values(loginError.current).forEach((item) => {
+          if (isEmpty(item)) return
+          toast.error(item, {
+            position: "bottom-right",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "light",
+          });
+        })
+      }
       // navigate("/");
     } catch (error) {
-      console.log({ error })
+      toast.error(error, {
+        position: "bottom-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
     }
   }
 
   const onSubmitSignup = (e) => {
     e.preventDefault()
-    if (signupInfo.password !== signupInfo.repeatPassword) return
-    dispatch(signup(signupInfo, navigate))
+    handleValidateSignup()
+    if (validateSignup.current) {
+      if (signupInfo.password !== signupInfo.repeatPassword) return
+      dispatch(signup(signupInfo, navigate))
+    }
+    else {
+      toast.dismiss()
+      Object.values(signupError.current).forEach((item) => {
+        if (isEmpty(item)) return
+        toast.error(item, {
+          position: "bottom-right",
+          autoClose: 5000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "light",
+        });
+      })
+    }
     // navigate("/");
   }
 
@@ -79,6 +172,7 @@ const Auth = () => {
       case 1:
         return (
           <div className="signup-form-container">
+            <ToastContainer />
             <p className="auth-form-title">Chào mừng tới FoodTown</p>
             <p>Hãy đăng ký để cùng ăn với chúng tôi</p>
             <div className="signup-form">
@@ -147,6 +241,7 @@ const Auth = () => {
       default:
         return (
           <form action="" className="login-form-container">
+            <ToastContainer />
             <p className="auth-form-title">Chào mừng tới FoodTown</p>
             <p>Hãy đăng nhập để cùng ăn với chúng tôi</p>
             <div className="login-form">
